34f20262a41ed4cf27e5b078f0716ffd2e7ebf7f
mouhamad0011 <139453293+mouhamad0011@users.noreply.github.com>
2023-07-28 12:00:33 +0300

    task10 done

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-let numbers =[1,2,3,4,5,6,7,8,9,10];
-for(let i=0;i<numbers.length;i++){
-    if(numbers[i] %2==0)console.log(numbers[i]+" is even");
-    else console.log(numbers[i]+" is odd");
+let modal=document.querySelector(".modal");
+let but=document.getElementById("openBtn");
+let modaltitle=document.getElementById("modalTitle");
+let closeBtn=document.getElementById("closeBtn");
+but.addEventListener("click",open);
+closeBtn.addEventListener("click",close);
+function open(){
+    modal.style.display="block";
 }
+function close(){
+    modal.style.display="none";
+}
+let title=prompt("Enter the title of modal: ");
+modalTitle.innerHTML=title;
+window.addEventListener('click', function(ref) {
+    if (ref.target == modal) {
+      modal.style.display = 'none';
+    }
+  });
